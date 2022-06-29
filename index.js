1b9c0a0f4bcbf5b260a6b5a65267b392b54330ec
KaptKeelhaul <96278047+KaptKeelhaul@users.noreply.github.com>
2022-06-29 05:27:27 +0000

    nothing

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
+const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-  res.send(index.html)
+// sendFile will go here
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
 });
 
-app.listen(3000, () => {
-  console.log('server started');
-});
+app.listen(port);
+console.log('Server started at http://localhost/:' + port);
+//Bazinga
