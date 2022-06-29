c811c2f6ccc6f3c3546213214573b36a53cffaaf
KaptKeelhaul <96278047+KaptKeelhaul@users.noreply.github.com>
2022-06-29 09:55:15 +0000

    Started work on generating a table

--- public/script.js
+++ public/script.js
@@ -14,3 +14,35 @@ function countDown()
 	var newVal = currentVal - 1;
 	document.getElementById("counterText").innerHTML = newVal;
 }
+
+
+//Beginnings of a table generator/injector
+
+//Pretending to be a database
+let tableData =
+  [
+    {row: 0, test: "Test 0"},
+		{row: 1, test: "Test 1"},
+		{row: 2, test: "Test 2"},
+		{row: 3, test: "Test 3"}
+	];
+
+function test()
+{
+	for (i = 0; i < tableData.length; i++)
+	{
+		//Adds html to the table with the id of "Bazinga" and then injects
+		//the "database" data into the table
+		document.getElementById("Bazinga").innerHTML += 
+					`
+					<tr>
+							<td> 
+								${tableData[i].row}
+							</td>
+							<td> 
+								${tableData[i].test}
+							</td>
+					</tr>
+					`;
+	}
+}
