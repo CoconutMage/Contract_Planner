061695d8aadb604694334ec3b878571ccf85af54
KaptKeelhaul <kaptainkeelhaul@gmail.com>
2022-07-01 21:59:03 -0400

    Added getting db info then injecting into tables

--- index.js
+++ index.js
@@ -27,61 +27,49 @@ function connectWebsocket(server)
 	wss.on('connection', (ws) => 
 	{
 		console.log("Websocket Connection Successful");
+		sendTableData();
+		
+		function sendTableData()
+		{
+			db.serialize(() => 
+			{
+				db.all('SELECT * FROM BudgetEstimate', /*[param1, param2],*/ (err, result) => 
+				{
+					if (err) 
+					{
+						console.log(err)
+					} 
+					else 
+					{
+						console.log(result);
+						let tableData = [];
+						tableData = result;
+						
+						wss.clients.forEach((client) => 
+						{
+							client.send(JSON.stringify(tableData));
+						});
+					}
+				});
+			});
+			db.close();
+		}
 	});
 
+	/*
 	//When the websocket opens do...
 	wss.onopen = function () 
 	{
 		//...nothing, yet
 	}	
+*/
 
 	//If the websocket throws an error, log the error
 	wss.on('error', function (event)
-		{
-			console.log(event)
-		});
+	{
+		console.log(event)
+	});
 }
-
-//Database shenanigans, I think I mostly know whats going on here
-db.serialize(() => {
-
-//const result = await db.get('SELECT col FROM tbl WHERE col = ?', 'test')
-	
-//db.run("INSERT INTO BudgetEstimate SET Item = Bazinga WHERE rowid = 1");
-
-/*
-db.run("INSERT INTO 'Budget Estimate'(Item) VALUES (:col)", {
-  ':1': 'Bazinga'
-});
-
-	
-
-	//console.log(db.get("SELECT * FROM 'Budget Estimate' WHERE rowid = 0"));
-console.log("It got here");
-	    db.each("SELECT rowid AS id, Item FROM 'Budget Estimate'", (err, row) => {
-        console.log(row.id + ": " + row.Item);
-				console.log(err);
-			});
-	
-	/*
-    db.run("CREATE TABLE lorem (info TEXT)");
-
-    const stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-    for (let i = 0; i < 10; i++) {
-        stmt.run("Ipsum " + i);
-    }
-    stmt.finalize();
-*
-		db.run("UPDATE lorem SET rowid = 1 WHERE rowid = 11 ");
-	
-    db.each("SELECT rowid AS id, info FROM lorem", (err, row) => {
-        console.log(row.id + ": " + row.info);
-    });
-*/
-});
-
-db.close();
-
 
 //This I think gets specific files, but its not needed atm cause public dir is all included
 /*app.get('/', function(req, res) {
--- public/script.js
+++ public/script.js
@@ -1,12 +1,39 @@
+let tableData = [];
+
 function Start()
 {
 	websocket();
-	generateTable();
+}
+
+function websocket()
+{
+	var HOST = location.origin.replace(/^http/, "ws");
+	var ws = new WebSocket(HOST);
+
+	//ws.addEventListener('open', () => {	});
+	ws.onopen = function (event)
+	{
+		//ws.send('Bazinga');
+	};
+
+	ws.onerror = function (event)
+	{
+		ws.send("ERROR " + event);
+	};
+
+	ws.onmessage = function (event)
+	{
+		data = [];
+		data = event.data;
+
+		tableData = JSON.parse(data);
+		generateTable();
+	};
 }
 
 function revealMessage()
 {
-	document.getElementById("hiddenMessage").style.display = 'block';
+	document.getElementById("hiddenMessage").style.display = "block";
 }
 function countUp()
 {
@@ -21,157 +48,110 @@ function countDown()
 	document.getElementById("counterText").innerHTML = newVal;
 }
 
-
-
-//This is a giant mess, I need to clean it up, and find a better way to either emulate a database
-//Or actually start using the database
-//Beginnings of a table generator/injector
-
-//Pretending to be a database
-let tableData =
-  [
-    {row: 0, test: "Project 1", test2: "$400", test3 : "$200", test4: "4/16/22"},
-		{row: 1, test: "Project 2", test2: "$6500", test3 : "$450", test4: "2/14/22"},
-		{row: 2, test: "Project 3", test2: "$900", test3 : "$20", test4: "3/07/22"},
-		{row: 3, test: "Project 4", test2: "$12,000", test3 : "$1490", test4: "10/28/21"}
-	];
-
 let isTableGenerated = false;
 function generateTable()
 {
-	//document.getElementById("Bazinga").innerHTML = '<tr class = "tableRow">';
-	
-	if(isTableGenerated == false)
+	/*
+	const tableBody = document.getElementById("bazingaTable2");
+	//const tableHead = document.getElementById('tableHead');
+	let dataHtml = "<tr>";
+	//let dataTableHead = '<tr>';
+	*/
+
+
+	if (isTableGenerated == false)
 	{
 		//Adds html to the table with the id of "Bazinga" and then injects
 		//the "database" data into the table
 		for (i = 0; i < tableData.length; i++)
 		{
-			document.getElementById("Bazinga").innerHTML +=
-					`
+			document.getElementById("Bazinga").innerHTML += `
 					<tr class = "tableRow">
 							<td id = "Bazinga"> 
-								${tableData[i].row}
+								${tableData[i].Item}
 							</td>
 							<td id = "Bazinga"> 
-								${tableData[i].test}
+								${tableData[i].Quantity}
 							</td>
 							<td id = "Bazinga"> 
-								${tableData[i].test2}
+								${tableData[i].Cost}
 							</td>
 							<td id = "Bazinga"> 
-								${tableData[i].test3}
+								${tableData[i].SubcontractorFee}
 							</td>
 							<td id = "Bazinga"> 
-								${tableData[i].test4}
+								${tableData[i].MaterialCost}
 							</td>
 						</tr>
 					`;
-      document.getElementById("Bazinga2").innerHTML +=
-					`
+			document.getElementById("Bazinga2").innerHTML += `
 					<tr class = "tableRow">
 							<td id = "Bazinga2"> 
-								${tableData[i].row}
+								${tableData[i].Item}
 							</td>
 							<td id = "Bazinga2"> 
-								${tableData[i].test}
+								${tableData[i].Quantity}
 							</td>
 							<td id = "Bazinga2"> 
-								${tableData[i].test2}
+								${tableData[i].Cost}
 							</td>
 							<td id = "Bazinga2"> 
-								${tableData[i].test3}
+								${tableData[i].SubcontractorFee}
 							</td>
 							<td id = "Bazinga2"> 
-								${tableData[i].test4}
+								${tableData[i].MaterialCost}
 							</td>
 						</tr>
 					`;
-      document.getElementById("Bazinga3").innerHTML +=
-					`
+			document.getElementById("Bazinga3").innerHTML += `
 					<tr class = "tableRow">
 							<td id = "Bazinga3"> 
-								${tableData[i].row}
+								${tableData[i].Item}
 							</td>
 							<td id = "Bazinga3"> 
-								${tableData[i].test}
+								${tableData[i].Quantity}
 							</td>
 							<td id = "Bazinga3"> 
-								${tableData[i].test2}
+								${tableData[i].Cost}
 							</td>
 							<td id = "Bazinga3"> 
-								${tableData[i].test3}
+								${tableData[i].SubcontractorFee}
 							</td>
 							<td id = "Bazinga3"> 
-								${tableData[i].test4}
+								${tableData[i].MaterialCost}
 							</td>
 						</tr>
 					`;
 			isTableGenerated = true;
 		}
-	}
-	else
+	} else
 	{
 		isTableGenerated = false;
 		document.getElementById("Bazinga").innerHTML = "<tr></tr>";
-    document.getElementById("Bazinga2").innerHTML = "<tr></tr>";
-    document.getElementById("Bazinga3").innerHTML = "<tr></tr>";
+		document.getElementById("Bazinga2").innerHTML = "<tr></tr>";
+		document.getElementById("Bazinga3").innerHTML = "<tr></tr>";
 		generateTable();
 	}
-	
+
 	//document.getElementById("Bazinga").innerHTML += '</tr>';
 	//isTableGenerated = true;
-}
-
-function addTableRow()
-{
-	document.getElementById("Bazinga").innerHTML = "<tr></tr>";
-	//I know this doesnt actually add or remove lines but 
-	//delete tableData[4];
-	tableData =
-	[
-		{row: 0, test: "Test 0", test2: "Bazinga", test3 : "Bazinga", test4: "Bazinga"},
-		{row: 1, test: "Test 1", test2: "Bazinga", test3 : "Bazinga", test4: "Bazinga"},
-		{row: 2, test: "Test 2", test2: "Bazinga", test3 : "Bazinga", test4: "Bazinga"},
-		{row: 3, test: "Test 3", test2: "Bazinga", test3 : "Bazinga", test4: "Bazinga"},
-		{row: 4, test: "Test 4", test2: "Bazinga", test3 : "Bazinga", test4: "Bazinga"},
-	];
-	isTableGenerated = false;
-	generateTable();
 }
 
 //code for the collapsible thing
 var coll = document.getElementsByClassName("collapsible");
 
-for (let i = 0; i < coll.length; i++) 
+for (let i = 0; i < coll.length; i++)
 {
-	coll[i].addEventListener("click", function() 
+	coll[i].addEventListener("click", function ()
 	{
 		this.classList.toggle("active");
 		var content = this.nextElementSibling;
-		if (content.style.display === "block") 
+		if (content.style.display === "block")
 		{
 			content.style.display = "none";
-		} 
-		else 
+		} else
 		{
 			content.style.display = "block";
 		}
 	});
-}
-
-function websocket() 
-{
-	var HOST = location.origin.replace(/^http/, 'ws')
-	var ws = new WebSocket(HOST);
-
-	//ws.addEventListener('open', () => {	});
-	ws.onopen = function(event) 
-	{
-		ws.send('Bazinga');
-	}
-	ws.onerror = function(event)
-		{
-			ws.send("ERROR " + event);
-		}
 }
